d57d48126806dabf8673931f26e2ce3266648bc1
SteveDeveloper8 <dasanza79@gmail.com>
2021-08-20 22:35:40 -0500

    Se agrego pantalla ConsultarUsuario

---
+++ Visual/Usuario/FrmConsultarUsuario.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace Visual.Usuario
+{
+    partial class FrmConsultarUsuario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.guna2HtmlLabel7 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.guna2PictureBox1 = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.guna2Elipse1 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
+            this.btnBuscar = new Guna.UI2.WinForms.Guna2GradientButton();
+            this.dgvReclusos = new Guna.UI2.WinForms.Guna2DataGridView();
+            this.txtCedula = new Guna.UI2.WinForms.Guna2TextBox();
+            this.lblCedula = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.btnMostrar = new Guna.UI2.WinForms.Guna2GradientButton();
+            this.ColNom = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColApe = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colUsuario = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colContraseña = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colRol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvReclusos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // guna2HtmlLabel7
+            // 
+            this.guna2HtmlLabel7.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel7.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2HtmlLabel7.ForeColor = System.Drawing.Color.White;
+            this.guna2HtmlLabel7.Location = new System.Drawing.Point(207, 23);
+            this.guna2HtmlLabel7.Name = "guna2HtmlLabel7";
+            this.guna2HtmlLabel7.Size = new System.Drawing.Size(233, 34);
+            this.guna2HtmlLabel7.TabIndex = 47;
+            this.guna2HtmlLabel7.Text = "Consulta de Usuario";
+            this.guna2HtmlLabel7.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
+            // 
+            // guna2PictureBox1
+            // 
+            this.guna2PictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2PictureBox1.FillColor = System.Drawing.Color.Transparent;
+            this.guna2PictureBox1.Image = global::Visual.Properties.Resources.user;
+            this.guna2PictureBox1.ImageRotate = 0F;
+            this.guna2PictureBox1.Location = new System.Drawing.Point(128, 12);
+            this.guna2PictureBox1.Name = "guna2PictureBox1";
+            this.guna2PictureBox1.ShadowDecoration.Parent = this.guna2PictureBox1;
+            this.guna2PictureBox1.Size = new System.Drawing.Size(62, 60);
+            this.guna2PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.guna2PictureBox1.TabIndex = 46;
+            this.guna2PictureBox1.TabStop = false;
+            // 
+            // guna2Elipse1
+            // 
+            this.guna2Elipse1.TargetControl = this;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.BackColor = System.Drawing.Color.Transparent;
+            this.btnBuscar.BorderRadius = 20;
+            this.btnBuscar.CheckedState.Parent = this.btnBuscar;
+            this.btnBuscar.CustomImages.Parent = this.btnBuscar;
+            this.btnBuscar.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnBuscar.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnBuscar.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnBuscar.DisabledState.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnBuscar.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnBuscar.DisabledState.Parent = this.btnBuscar;
+            this.btnBuscar.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnBuscar.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnBuscar.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold);
+            this.btnBuscar.ForeColor = System.Drawing.Color.White;
+            this.btnBuscar.HoverState.Parent = this.btnBuscar;
+            this.btnBuscar.Location = new System.Drawing.Point(418, 96);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.ShadowDecoration.Parent = this.btnBuscar;
+            this.btnBuscar.Size = new System.Drawing.Size(125, 38);
+            this.btnBuscar.TabIndex = 51;
+            this.btnBuscar.Text = "Buscar";
+            // 
+            // dgvReclusos
+            // 
+            this.dgvReclusos.AllowUserToAddRows = false;
+            this.dgvReclusos.AllowUserToDeleteRows = false;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.White;
+            this.dgvReclusos.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvReclusos.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvReclusos.BackgroundColor = System.Drawing.Color.WhiteSmoke;
+            this.dgvReclusos.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dgvReclusos.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+            this.dgvReclusos.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.DarkOrange;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 10.5F);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvReclusos.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvReclusos.ColumnHeadersHeight = 21;
+            this.dgvReclusos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColNom,
+            this.ColApe,
+            this.colUsuario,
+            this.colContraseña,
+            this.colRol});
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Segoe UI", 10.5F);
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvReclusos.DefaultCellStyle = dataGridViewCellStyle3;
+            this.dgvReclusos.EnableHeadersVisualStyles = false;
+            this.dgvReclusos.GridColor = System.Drawing.Color.White;
+            this.dgvReclusos.Location = new System.Drawing.Point(36, 163);
+            this.dgvReclusos.Name = "dgvReclusos";
+            this.dgvReclusos.ReadOnly = true;
+            this.dgvReclusos.RowHeadersVisible = false;
+            this.dgvReclusos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvReclusos.Size = new System.Drawing.Size(495, 217);
+            this.dgvReclusos.TabIndex = 50;
+            this.dgvReclusos.ThemeStyle.AlternatingRowsStyle.BackColor = System.Drawing.Color.White;
+            this.dgvReclusos.ThemeStyle.AlternatingRowsStyle.Font = null;
+            this.dgvReclusos.ThemeStyle.AlternatingRowsStyle.ForeColor = System.Drawing.Color.Empty;
+            this.dgvReclusos.ThemeStyle.AlternatingRowsStyle.SelectionBackColor = System.Drawing.Color.Empty;
+            this.dgvReclusos.ThemeStyle.AlternatingRowsStyle.SelectionForeColor = System.Drawing.Color.Empty;
+            this.dgvReclusos.ThemeStyle.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.dgvReclusos.ThemeStyle.GridColor = System.Drawing.Color.White;
+            this.dgvReclusos.ThemeStyle.HeaderStyle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.dgvReclusos.ThemeStyle.HeaderStyle.BorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.dgvReclusos.ThemeStyle.HeaderStyle.Font = new System.Drawing.Font("Segoe UI", 10.5F);
+            this.dgvReclusos.ThemeStyle.HeaderStyle.ForeColor = System.Drawing.Color.White;
+            this.dgvReclusos.ThemeStyle.HeaderStyle.HeaightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
+            this.dgvReclusos.ThemeStyle.HeaderStyle.Height = 21;
+            this.dgvReclusos.ThemeStyle.ReadOnly = true;
+            this.dgvReclusos.ThemeStyle.RowsStyle.BackColor = System.Drawing.Color.White;
+            this.dgvReclusos.ThemeStyle.RowsStyle.BorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+            this.dgvReclusos.ThemeStyle.RowsStyle.Font = new System.Drawing.Font("Segoe UI", 10.5F);
+            this.dgvReclusos.ThemeStyle.RowsStyle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            this.dgvReclusos.ThemeStyle.RowsStyle.Height = 22;
+            this.dgvReclusos.ThemeStyle.RowsStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.dgvReclusos.ThemeStyle.RowsStyle.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            // 
+            // txtCedula
+            // 
+            this.txtCedula.BackColor = System.Drawing.Color.Transparent;
+            this.txtCedula.BorderColor = System.Drawing.Color.Silver;
+            this.txtCedula.BorderRadius = 15;
+            this.txtCedula.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtCedula.DefaultText = "";
+            this.txtCedula.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtCedula.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtCedula.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtCedula.DisabledState.Parent = this.txtCedula;
+            this.txtCedula.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtCedula.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtCedula.FocusedState.Parent = this.txtCedula;
+            this.txtCedula.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.txtCedula.ForeColor = System.Drawing.Color.Black;
+            this.txtCedula.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtCedula.HoverState.Parent = this.txtCedula;
+            this.txtCedula.Location = new System.Drawing.Point(150, 96);
+            this.txtCedula.Name = "txtCedula";
+            this.txtCedula.PasswordChar = '\0';
+            this.txtCedula.PlaceholderText = "";
+            this.txtCedula.SelectedText = "";
+            this.txtCedula.ShadowDecoration.Parent = this.txtCedula;
+            this.txtCedula.Size = new System.Drawing.Size(261, 36);
+            this.txtCedula.TabIndex = 49;
+            // 
+            // lblCedula
+            // 
+            this.lblCedula.BackColor = System.Drawing.Color.Transparent;
+            this.lblCedula.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCedula.ForeColor = System.Drawing.Color.White;
+            this.lblCedula.Location = new System.Drawing.Point(51, 96);
+            this.lblCedula.Name = "lblCedula";
+            this.lblCedula.Size = new System.Drawing.Size(93, 32);
+            this.lblCedula.TabIndex = 48;
+            this.lblCedula.Text = "Apellidos:";
+            this.lblCedula.TextAlignment = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.BackColor = System.Drawing.Color.Transparent;
+            this.btnMostrar.BorderRadius = 20;
+            this.btnMostrar.CheckedState.Parent = this.btnMostrar;
+            this.btnMostrar.CustomImages.Parent = this.btnMostrar;
+            this.btnMostrar.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnMostrar.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnMostrar.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnMostrar.DisabledState.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnMostrar.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnMostrar.DisabledState.Parent = this.btnMostrar;
+            this.btnMostrar.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnMostrar.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnMostrar.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold);
+            this.btnMostrar.ForeColor = System.Drawing.Color.White;
+            this.btnMostrar.HoverState.Parent = this.btnMostrar;
+            this.btnMostrar.Location = new System.Drawing.Point(182, 386);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.ShadowDecoration.Parent = this.btnMostrar;
+            this.btnMostrar.Size = new System.Drawing.Size(190, 38);
+            this.btnMostrar.TabIndex = 52;
+            this.btnMostrar.Text = "Mostrar Todos";
+            // 
+            // ColNom
+            // 
+            this.ColNom.HeaderText = "Nombres";
+            this.ColNom.Name = "ColNom";
+            this.ColNom.ReadOnly = true;
+            // 
+            // ColApe
+            // 
+            this.ColApe.HeaderText = "Apellidos";
+            this.ColApe.Name = "ColApe";
+            this.ColApe.ReadOnly = true;
+            // 
+            // colUsuario
+            // 
+            this.colUsuario.HeaderText = "Usuario";
+            this.colUsuario.Name = "colUsuario";
+            this.colUsuario.ReadOnly = true;
+            // 
+            // colContraseña
+            // 
+            this.colContraseña.HeaderText = "Contraseña";
+            this.colContraseña.Name = "colContraseña";
+            this.colContraseña.ReadOnly = true;
+            // 
+            // colRol
+            // 
+            this.colRol.HeaderText = "Rol";
+            this.colRol.Name = "colRol";
+            this.colRol.ReadOnly = true;
+            // 
+            // FrmConsultarUsuario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Visual.Properties.Resources.ReclusoBG;
+            this.ClientSize = new System.Drawing.Size(568, 440);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.dgvReclusos);
+            this.Controls.Add(this.txtCedula);
+            this.Controls.Add(this.lblCedula);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.guna2HtmlLabel7);
+            this.Controls.Add(this.guna2PictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmConsultarUsuario";
+            this.Text = "FrmConsultarUsuario";
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvReclusos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel7;
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox1;
+        private Guna.UI2.WinForms.Guna2Elipse guna2Elipse1;
+        private Guna.UI2.WinForms.Guna2GradientButton btnBuscar;
+        private Guna.UI2.WinForms.Guna2DataGridView dgvReclusos;
+        private Guna.UI2.WinForms.Guna2TextBox txtCedula;
+        private Guna.UI2.WinForms.Guna2HtmlLabel lblCedula;
+        private Guna.UI2.WinForms.Guna2GradientButton btnMostrar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColNom;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColApe;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colUsuario;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colContraseña;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colRol;
+    }
+}
