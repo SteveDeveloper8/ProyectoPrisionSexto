45731dafa0ea4974e8ef90092f49be874785e278
SteveDeveloper8 <dasanza79@gmail.com>
2021-08-20 22:21:35 -0500

    Se agrego constructor a PrincipalAlcaide

--- Visual/PrincipalAlcaide.cs
+++ Visual/PrincipalAlcaide.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Control;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,11 +13,53 @@ namespace Visual
 {
     public partial class PrincipalAlcaide : Form
     {
+        ControlRecluso controlRecluso = new ControlRecluso();
         public PrincipalAlcaide()
         {
             InitializeComponent();
+            consultarReclusos();
         }
 
+        private void consultarReclusos()
+        {
+            List<Object> reclusos = null;
+            try
+            {
+                reclusos = controlRecluso.ListarReclusos();
+                LlenarTablaReclusos(reclusos);
+            }
+            catch (GeneralExcepcion ex)
+            {
+                MessageBox.Show("No hay reclusos registrados en el sistema.");
+            }
+        }
+
+        private void LlenarTablaReclusos(List<Object> reclusos)
+        {
+            LimpiarTabla();
+            foreach (var recluso in reclusos)
+            {
+                InsertarFila(recluso);
+            }
+            dgvReclusos.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            dgvReclusos.AutoResizeColumns();
+        }
+        private void InsertarFila(Object recluso)
+        {
+            Type tipo = recluso.GetType();
+            string codigo = (string)tipo.GetProperty("Codigo").GetValue(recluso);
+            string nombre = (string)tipo.GetProperty("Nombre").GetValue(recluso);
+            string apellidos = (string)tipo.GetProperty("Apellidos").GetValue(recluso);
+            string genero = (string)tipo.GetProperty("Genero").GetValue(recluso);
+            string fecha = ((DateTime)tipo.GetProperty("Fecha").GetValue(recluso)).ToString("dd/MM/yyyy");
+            string cedula = (string)tipo.GetProperty("Cedula").GetValue(recluso);
+            dgvReclusos.Rows.Add(codigo, nombre, apellidos, cedula, genero, fecha, "Ver expediente");
+        }
+
+        private void LimpiarTabla()
+        {
+            dgvReclusos.Rows.Clear();
+        }
         private void toolStripMenuItem1_Click(object sender, EventArgs e)
         {
 
